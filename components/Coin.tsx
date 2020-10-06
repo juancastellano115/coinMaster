f26befabf14e22455661322126e0a2f3f56077af
juancastellano115 <juancastellano115@gmail.com>
2020-10-06 20:13:36 +0200

    structure + coins + currencyList

---
+++ components/Coin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native-paper'
+
+export default function Coin() {
+    return (
+        <Text>
+            moneda girando re shulona
+        </Text>
+    )
+}
